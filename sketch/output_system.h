ada617de01d308fe5e932abb7664a09f6d23c861
Phumipas Namjaidee <phumihimal@gmail.com>
2025-05-06 19:39:26 +0700

    feat: interface output

--- sketch/output_system.h
+++ sketch/output_system.h
@@ -1,4 +1,5 @@
 #include <LiquidCrystal.h>
+#include <Arduino.h>
 // const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 // LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
@@ -12,6 +13,11 @@ public:
     LiquidCrystal lcdDisplay;
 
     OutputSystem(int buzzerPin, int ledPin, LiquidCrystal lcdDisplay);
+
+    void setBuzzerLED(int value); //value = LOW or HIGH
+    void setLCD(String name);
+
+    
 };
 
 #endif
--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,19 +1,23 @@
 #include "global-flag.h"
 #include "output_system.h"
+#include <Arduino.h>
 #include <LiquidCrystal.h>
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+// const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+// LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 const int buzzerPin = 6;
 const int ledPin = 7;
 
+bool isPressing = false;
+
 void setup() {
   Serial.begin(9600);
-  OutputSystem outputSys(buzzerPin, ledPin, lcd);
-  Serial.print(outputSys.buzzerPin);
+  // OutputSystem outputSys(buzzerPin, ledPin, lcd);
+  // Serial.print(outputSys.buzzerPin);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  int buttonState = digitalRead(buzzerPin);
 }
