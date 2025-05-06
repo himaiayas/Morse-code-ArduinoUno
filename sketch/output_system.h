9a5e6f92bbb91c6e340e4f4d677eeabf70dd594e
Phumipas Namjaidee <phumihimal@gmail.com>
2025-05-06 19:45:16 +0700

    fix: argument name changed

--- sketch/output_system.h
+++ sketch/output_system.h
@@ -15,7 +15,7 @@ public:
     OutputSystem(int buzzerPin, int ledPin, LiquidCrystal lcdDisplay);
 
     void setBuzzerLED(int value); //value = LOW or HIGH
-    void setLCD(String name);
+    void setLCD(String word);
 
     
 };
