8a651cb239433d5820e365cf46db009add570454
Pankham <pankhumpattakulpong@gmail.com>
2025-05-07 15:20:42 +0700

    add function setbuzzer and lcd

--- sketch/output-system.cpp
+++ sketch/output-system.cpp
@@ -3,5 +3,15 @@
 OutputSystem::OutputSystem(int buzzerPin, int ledPin, LiquidCrystal lcdDisplay):
   buzzerPin(buzzerPin),
   ledPin(ledPin),
-  lcdDisplay(lcdDisplay)
+  lcdDisplay(lcdDisplay),
+  void OutputSystem::setBuzzerLED(int value){
+    digitalWrite(buzzerPin,value);
+    digitalWrite(ledPin,value);
+  },
+  void OutputSystem::setLCD(String message){
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print(message);
+  }
+
   {};
