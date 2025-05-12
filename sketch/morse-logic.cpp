353740d948c5533103134e5cde9dd0d1dadee55f
Pankham <pankhumpattakulpong@gmail.com>
2025-05-12 18:22:54 +0700

    add function and letter

--- sketch/morse-logic.cpp
+++ sketch/morse-logic.cpp
@@ -6,11 +6,20 @@
 static const MorseEntry MorseLogic::morseEncoder[26]={
   {'E',"."},
   {'T',"_"},
-  //TODO: insert all {letter,morseCode} for each letter;
+  {'A',"_"},//TODO: insert all {letter,morseCode} for each letter;
+  {'B,"_'},{"C","_"},{"D","_"},{'F,"_'},{"G","_"},{"H","_"},
+  {'I,"_'},{"J","_"},{"K","_"}{'L,"_'},{"M","_"},{"N","_"},
+  {'O,"_'},{"P","_"},{"Q","_"},{'R,"_'},{"S","_"},{"U","_"},
+  {'V,"_'},{"W","_"},{"X","_"}{'Y,"_'},{"Z","_"};
 };
-
+const int morseTableSize=sizeof(morseTable)/sizeof(MorseEntry);
 static String MorseLogic::encode(char letter){
-  //TODO: implement this function.
+  input=std::toupper(static_cast<unsigned char>(letter));
+  for(int i=0;i<morseTableSize;i++){
+    if(morseTable[i].letter==letter){
+      return morseTable[i].code;
+    }
+  }
   //make sure letter is uppercase
   //find matching letter in morseEncoder array and return morse code
   //Note* example if(morseEncoder[0].letter == 'E') return morseEncoder[0].code;
